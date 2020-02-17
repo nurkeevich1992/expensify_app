14fa9f540642937d54e6b1a7d41e5e892cbfe042
K-Bermet <bkalbotoeva@gmail.com>
2020-02-17 12:25:51 -0600

    project starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import IndecisionApp from "./components/IndecisionApp";
 import "./styles/styles.scss";
 import "normalize.css/normalize.css";
 
 const App = () => {
-    return <IndecisionApp />;
+    return <p>This is my boilerplate</p>;
 };
 
 export default App;
