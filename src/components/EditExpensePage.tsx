dd4cb76be78da6e8403fa3e93970636e51cfcc77
K-Bermet <bkalbotoeva@gmail.com>
2020-02-29 10:37:01 -0600

    update docker compose and Edit EditExpensePage

--- src/components/EditExpensePage.tsx
+++ src/components/EditExpensePage.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import * as history from "history";
 
-interface EditExpensePageProps {
-    location?: history.Location;
-    history?: history.History;
-}
+// interface EditExpensePageProps {
+//     location?: history.Location;
+//     history?: history.History;
+// }
 
-const EditExpensePage = (props: EditExpensePageProps) => {
+const EditExpensePage: React.FC = props => {
+    console.log("====================================");
+    console.log(props);
+    console.log("====================================");
     return <div>This is from my Edit expense page</div>;
 };
 
